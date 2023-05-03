533e51eef2d74a91b59f758e1a0a1300cca479c8
HD <engineeer.h@gmail.com>
2023-05-04 02:42:18 +0500

    seconf commit

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # rock-paper-scissors
+
+## Introduction:
+The rock-paper-scissors game is a simple game that involves two players, each choosing one of three possible options: rock, paper, or scissors. The game is usually played between two people, but it can also be played between a person and a computer. The game is based on the idea that rock beats scissors, scissors beat paper, and paper beats rock.
+
+## Functions:
+
+### 
+computerChoiceGenerator(): This function generates a random choice for the computer using the Math.random() function. The function selects one of three possible choices: rock, paper, or scissors.
+
+###
+getResult(): This function determines the winner of the game based on the choices made by the user and the computer. The function uses conditional statements to compare the choices and determine the winner.
+
+###
+possibleChoices.forEach((button) => {...}): This function uses a forEach loop to add an event listener to each button. The event listener is triggered when the user clicks on one of the buttons. The function then calls the computerChoiceGenerator() function to generate a choice for the computer. The function then calls the getResult() function to determine the winner of the game.
+
+###
+document.getElementById("computer-choice"): This function retrieves the computer-choice element from the HTML document and assigns it to the computerChoiceDisplay variable. The function is used to display the computer's choice on the web page.
+
+###
+document.getElementById("user-choice"): This function retrieves the user-choice element from the HTML document and assigns it to the userChoiceDisplay variable. The function is used to display the user's choice on the web page.
+
+###
+document.getElementById("result"): This function retrieves the result element from the HTML document and assigns it to the resultDisplay variable. The function is used to display the result of the game on the web page.
+
+##
+Conclusion:
+The rock-paper-scissors game is a simple yet entertaining game that can be played between two people or between a person and a computer. This project demonstrates how HTML, CSS, and JavaScript can be used to create a functioning game that is easy to play and understand. The game is a great way to practice programming skills and experiment with different coding techniques.
--- app.js
+++ app.js
@@ -1,66 +1,55 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+
 //Declaring Global Variables
-let userChoice
-let result
-let computerChoice
+let userChoice;
+let result;
+let computerChoice;
 
 // Use forEach loop to add an event listener to each button
-possibleChoices.forEach(button => {
-  button.addEventListener('click', (e) => {
-   
-    userChoice =  e.target.id
-    userChoiceDisplay.innerHTML = userChoice
+possibleChoices.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
     //Calling computerChoice Function
-     computerChoice = computerChoiceGenerator()
-    computerChoiceDisplay.innerHTML = computerChoice
+    computerChoice = computerChoiceGenerator();
+    computerChoiceDisplay.innerHTML = computerChoice;
 
-    let result = getResult()
+    let result = getResult();
 
-    resultDisplay.innerHTML = result
-
-
+    resultDisplay.innerHTML = result;
   });
 });
 
-
-
-
- 
 function computerChoiceGenerator() {
-    const choices = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-    
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "its a draw";
   }
-
-
-
-function getResult(){
-    if(computerChoice===userChoice){
-       result = "its a draw"
-    }
-    if(computerChoice==='rock' && userChoice ==='paper'){
-       result = 'you win' 
-    }
-    if(computerChoice==='rock' && userChoice ==='scissors'){
-        result = 'you lost' 
-     }
-     if(computerChoice==='paper' && userChoice ==='scissors'){
-        result = 'you win' 
-     }
-     if(computerChoice==='paper' && userChoice ==='rock'){
-        result = 'you lose' 
-     }
-     if(computerChoice==='scissors' && userChoice ==='rock'){
-        result = 'you win' 
-     }
-     if(computerChoice==='scissors' && userChoice ==='paper'){
-        result = 'you lose' 
-     }  
-     return result
-}  
-
- 
+  if (computerChoice === "rock" && userChoice === "paper") {
+    result = "you win";
+  }
+  if (computerChoice === "rock" && userChoice === "scissors") {
+    result = "you lost";
+  }
+  if (computerChoice === "paper" && userChoice === "scissors") {
+    result = "you win";
+  }
+  if (computerChoice === "paper" && userChoice === "rock") {
+    result = "you lose";
+  }
+  if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "you win";
+  }
+  if (computerChoice === "scissors" && userChoice === "paper") {
+    result = "you lose";
+  }
+  return result;
+}
